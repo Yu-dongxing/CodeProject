9ac5e550a5a0d6647b9b8c468f255fb2405cfb8b
yuxs <yuxs2022@163.com>
2022-12-14 19:03:08 +0800

    使用sum函数

--- prog6.c
+++ prog6.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+int main()
+{
+    
+    int ever = 567;
+    int mount = 32423;
+    int jg = ever + mount;
+    printf("%d",jg);
+
+    /* 使用sum函数 */
+    return 0;
+}

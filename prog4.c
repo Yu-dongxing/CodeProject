421effeb42a6388d2e2e6b69906e997d1d437800
yuxs <yuxs2022@163.com>
2022-12-19 18:55:18 +0800

    更改整数变小数

--- prog4.c
+++ prog4.c
@@ -8,8 +8,7 @@ int main()
 
     scanf("%d %d", &foot, &inch);
 
-    printf("身高是%f米。\n",((foot + inch / 12) * 0.3048));
-
+    printf("身高是%f米。\n",((foot + inch / 12.0) * 0.3048));
     return 0;
 
 }

fa9a63faf90196a8e10f113ee04a73a99c6b34fb
yuxs <yuxs2022@163.com>
2023-02-19 22:06:23 +0800

    mooc 6

--- .vscode/mooc4.c
+++ .vscode/mooc4.c
@@ -3,11 +3,18 @@
 int main(){
     int n;
     printf("请输入1900-2200之间的年份；");
+
     scanf("%d",&n);
+
     if (n>2200||n<1900)printf ("error");
+
     else {if((n%400==0)||(n%4==0&&n%100!=0))
+
     printf("yes\n");
-    else printf("no\n");}
+
+    else printf("no\n");
+    
+    }
     return 0;
 
 }
---
+++ .vscode/mooc5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int x,y,z,m;
+
+    scanf("%d%d%d",&x,&y,&z);
+    if(x>0&&y>0||z>=0){
+        if(y>z*50){
+            m=x+y-50*z;
+        }
+        else{
+            m=x
+        }
+        printf("%d",m);
+    }else{
+        printf("error");
+    }
+    return 0;
+}

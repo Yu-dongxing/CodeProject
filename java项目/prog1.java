ee1471a839c0e0a35e1ec6e964ff6472294a6a11
yuxs <yuxs2022@163.com>
2023-03-09 14:54:31 +0800

    vs

---
+++ java项目/prog1.java
@@ -0,0 +1,8 @@
+public class prog1 {
+    /* 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // 输出 Hello World
+    }
+}

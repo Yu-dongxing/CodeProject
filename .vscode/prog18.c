2874a7adc4cc8f8b461fe964f4e54edbf7711902
yuxs <yuxs2022@163.com>
2023-08-14 17:04:21 +0800

    33

--- .vscode/prog18.c
+++ .vscode/prog18.c
@@ -1,15 +1,16 @@
 /*二进制转换为十进制*/
+#include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-int yuxs (long long n);
+int yuxs (long  n);
 int main(){
-    long long n;
+    long  n;
     printf("输入二进制：");
     scanf("%lld",&n);
     printf("二进制%lld转换为十进制为：%d",n,yuxs(n));
     return 0;
 }
-int yuxs(long long n){
+int yuxs(long  n){
     int dec = 0,i = 0,remainder;
     while (n!=0)
     {

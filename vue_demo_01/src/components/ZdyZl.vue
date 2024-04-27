85f995300f2630406ad00e55997e74f9404c677b
yuxs <14122291+yuxes@user.noreply.gitee.com>
2024-04-27 22:20:49 +0800

    添加自定义指令案例

---
+++ vue_demo_01/src/components/ZdyZl.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>自定义指令</h1>
+    <input  type="text" ref="inp">
+  </div>
+</template>
+
+<script>
+export default {
+    // mounted(){
+    //     this.$refs.inp.focus()
+    // }
+    
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_demo_01/src/components/ZlValue.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 v-color >指令的值</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    directives:{
+        color:{
+            inserted(el){
+                console.log(el);
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

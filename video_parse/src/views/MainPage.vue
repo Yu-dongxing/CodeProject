dea1a60962112299335862a3a67007e46bd35e2f
yuxs <14122291+yuxes@user.noreply.gitee.com>
2024-05-17 16:11:36 +0800

    代码重构操作，新建主页面

---
+++ video_parse/src/views/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="M_P">
+    <div class="P_a">
+      <div class="A_a"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MainPage'
+
+}
+</script>
+
+<style>
+.M_P{
+    width: 1400px;
+    height: 500px;
+    /* background-color: aqua; */
+    margin: 0 auto;
+    margin-top: 10px;
+    border-radius: 10px;
+}
+</style>

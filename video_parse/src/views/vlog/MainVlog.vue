1089a675451c32b31318321dfeaeceb5be775c89
yuxs <yuxs2022@163.com>
2024-06-22 09:22:27 +0800

    增加日志页面框架代码

--- video_parse/src/views/vlog/MainVlog.vue
+++ video_parse/src/views/vlog/MainVlog.vue
@@ -1,16 +1,31 @@
 <template>
+  <!-- 开发日志 -->
   <div class="MainVlog">
-    日志
+    <div class="M_V">
+    </div>
   </div>
 </template>
-
 <script>
 export default {
     name:'MainVlog',
-    
+    data(){
+      return{
+        Liist:[
+          {id:1,desc:"231231232"},
+        ]
+      }
+    },
+
 }
 </script>
 
 <style>
+.MainVlog{
 
+}
+.MainVlog .M_V{
+  width: 100%;
+  height: 200px;
+  background-color: black;
+}
 </style>

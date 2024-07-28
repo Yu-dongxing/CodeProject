1f29c2f7c3cbc8e68429f2d4be60961813665a67
yuxs <yuxs2022@163.com>
2024-07-28 21:08:27 +0800

    2024-7-28-1-yuxs-update-添加less框架

--- video_parse/src/main.js
+++ video_parse/src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router/index.js'
 import Store from './store/index.js'
+import less  from 'less'
 
 const app = createApp(App)  
 // 使用路由插件  
 app.use(Router)  
 app.use(Store)
+app.use(less)
   
 // 注册全局组件  
 // app.component('BaseA', BaseA)  
